4c1683bf4bc9dfa14907c60950bb5a1f2bc58af5
Pedro Rocha <pedrofrancisconeves@yahoo.com.br>
2022-10-01 17:19:06 -0300

    errata.c

--- Eleições/eleicoes.c
+++ Eleições/eleicoes.c
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<conio.h>
-#include<iostream>
 #include<math.h>
 
 
@@ -14,44 +13,44 @@ int main()
 
     while(v!=0)
     {
-        printf("\n\n 1-Candidato 1\n 2-Candidato 2\n 3 -Candidato 3\n 4-Candidato 4\n 5-Voto em branco\n 6-Voto Nulo");
+        printf("\n\n 1- Candidato 1\n 2- Candidato 2\n 3- Candidato 3\n 4- Candidato 4\n 5-Voto em branco\n 6- Voto Nulo");
         printf("\nDigite o numero correspondente ao seu voto e aperte ENTER: ");
-        scanf("%i", &v)
+        scanf("%i", &v);
 
         switch (v)
         {
         case 0: 
-        printf("\nVotação Encerrada"); 
+        printf("\nVotação Encerrada "); 
         break;
 
         case 1: 
         c1 = c1 + 1;
-        printf("\nVoto registrado!!");
+        printf("\nCandidato 1: Voto registrado!!");
         break;
 
          case 2: 
         c2 = c2 + 1;
-        printf("\nVoto registrado!!");
+        printf("\nCandidato 2: Voto registrado!!");
         break;
 
          case 3: 
         c3 = c3 + 1;
-        printf("\nVoto registrado!!");
+        printf("\nCandidato 3: Voto registrado!!");
         break;
 
          case 4: 
         c4 = c4 + 1;
-        printf("\nVoto registrado!!");
+        printf("\nCandidato 4: Voto registrado!!");
         break;
 
          case 5: 
         vn = vn + 1;
-        printf("\nVoto registrado!!");
+        printf("\nNulo: Voto registrado!!");
         break;
 
          case 6: 
         vb = vb + 1;
-        printf("\nVoto registrado!!");
+        printf("\nBranco: Voto registrado!!");
         break;
 
         default: 
@@ -59,7 +58,7 @@ int main()
         break;
         }
     }
-    printf("Resultados:\n Candidato 1\t %i\n Candidato 2\t %i\n Candidato 3\t %i\n Candidato 4\t %i\n Votos Nulos\t %i\n Votos em Branco\t %i\n",c1,c2,c3,c4,vn,vb);  
+    printf("Resultados:\n Candidato 1\t\t %i\n Candidato 2\t %i\n Candidato 3\t %i\n Candidato 4\t %i\n Votos Nulos\t %i\n Votos em Branco %i\n",c1,c2,c3,c4,vn,vb);  
 
 
 
